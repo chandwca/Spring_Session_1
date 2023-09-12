28eae77f0e0e1dacdc4d344c3874d40c855fd4e8
chandwca <chetnachandwani.01@gmail.com>
2023-09-12 17:10:51 -0400

    Custom init and destroy - lifecycle of Spring Bean

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,11 +1,24 @@
 package org.example;
 
 public class Customer {
+    //Constructor
+    public Customer()
+    {
+        System.out.println("Welcome");
+    }
     private String name;
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
+    }
+    // Custom initialization method
+    public void init() {
+        System.out.println("Customer bean is being initialized.");
+    }
+    // Custom destruction method
+    public void destroy() {
+        System.out.println("Customer bean is being destroyed.");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,21 @@
 package org.example;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         // Load the Spring configuration
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
         // Retrieve the "person" bean from the Spring container
-        Customer customer = (Customer) context.getBean("person");
+        Customer customer = (Customer) context.getBean("customer");
 
         // Get and display the Customer's name
         System.out.println("Customer's name: " + customer.getName());
+        // Close the Spring container
+        context.registerShutdownHook();
     }
+
+
 }
