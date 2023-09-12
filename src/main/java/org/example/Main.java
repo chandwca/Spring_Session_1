a6e95a4004f4cae7f3ef9dae40475aa081264c97
chandwca <chetnachandwani.01@gmail.com>
2023-09-12 15:28:51 -0400

    Chapter1-Bean creation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Load the Spring configuration
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        // Retrieve the "person" bean from the Spring container
+        Customer customer = (Customer) context.getBean("person");
+
+        // Get and display the Customer's name
+        System.out.println("Customer's name: " + customer.getName());
     }
 }
